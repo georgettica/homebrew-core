8678dc8a71afdaabd183a7556ed6865a0a1d7395
Ron Green <11993626+georgettica@users.noreply.github.com>
2023-09-03 19:04:30 +0300

    audit fix

--- Formula/a/audiowaveform.rb
+++ Formula/a/audiowaveform.rb
@@ -3,18 +3,17 @@ class Audiowaveform < Formula
   homepage "https://github.com/bbc/audiowaveform"
   url "https://github.com/bbc/audiowaveform/archive/1.8.1.tar.gz"
   sha256 "bd1254a4ddbc0eb68eb8dbd549335c3207260afdae4bf80cfe5d4129de51d1e7"
+  license "GPL-3.0-or-later"
+
+  # if MacOS.version < :mavericks  depends_on "boost"
+  # # => "c++11" end - commented out because of audit, maybe it's needed
+  depends_on "boost"
+
   depends_on "cmake"
   depends_on "gd"
   depends_on "libid3tag"
-  depends_on "libmad"
   depends_on "libsndfile"
-
-  if MacOS.version < :mavericks
-    depends_on "boost" => "c++11"
-  else
-    depends_on "boost"
-  end
-  # depends_on "gmock"
+  depends_on "mad"
 
   def install
     cmake_args = std_cmake_args
@@ -33,17 +32,17 @@ class Audiowaveform < Formula
   end
 
   test do
-      system bin/"audiowaveform", "--help"
-      if OS.linux?
-        system "wget", "https://github.com/georgettica/generate-random-audio/releases/download/v0.1.11/generate-random-audio_v0.1.11_x86_64-unknown-linux-musl.tar.gz"
-        system "tar", "xf", "generate-random-audio_v0.1.11_x86_64-unknown-linux-musl.tar.gz"
-      end
-      if OS.mac?
-        system "wget", "https://github.com/georgettica/generate-random-audio/releases/download/v0.1.11/generate-random-audio_v0.1.11_x86_64-apple-darwin.zip"
-        system "unzip", "generate-random-audio_v0.1.11_x86_64-apple-darwin.zip"
-      end
-      system "./generate-random-audio"
-      system "audiowaveform", "-i", "random_audio.wav", "-o", "random_audio.png"
-      system "file", "random_audio.png"
+    system bin/"audiowaveform", "--help"
+    if OS.linux?
+      system "wget", "https://github.com/georgettica/generate-random-audio/releases/download/v0.1.11/generate-random-audio_v0.1.11_x86_64-unknown-linux-musl.tar.gz"
+      system "tar", "xf", "generate-random-audio_v0.1.11_x86_64-unknown-linux-musl.tar.gz"
+    end
+    if OS.mac?
+      system "wget", "https://github.com/georgettica/generate-random-audio/releases/download/v0.1.11/generate-random-audio_v0.1.11_x86_64-apple-darwin.zip"
+      system "unzip", "generate-random-audio_v0.1.11_x86_64-apple-darwin.zip"
+    end
+    system "./generate-random-audio"
+    system bin/"audiowaveform", "-i", "random_audio.wav", "-o", "random_audio.png"
+    system "file", "random_audio.png"
   end
 end

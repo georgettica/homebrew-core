18424e74e076a494009dc0acb414152728c0b5c0
Ron Green <11993626+georgettica@users.noreply.github.com>
2023-12-06 19:52:35 +0200

    fix: use resource as intended?

--- Formula/a/audiowaveform.rb
+++ Formula/a/audiowaveform.rb
@@ -19,29 +19,28 @@ class Audiowaveform < Formula
     system "cmake", "--install", "build"
   end
 
-  def retrieve_audio_generator
-    on_linux do
-      resource "generate-random-audio" do
-        url "https://github.com/georgettica/generate-random-audio/releases/download/v0.1.11/generate-random-audio_v0.1.11_x86_64-unknown-linux-musl.tar.gz"
-        sha256 "a066c9b760010f8ac711b7c88bd4a566ed1ab789234f1633d35dc4db654aab0c"
-      end
+  on_linux do
+    resource "generate-random-audio" do
+      url "https://github.com/georgettica/generate-random-audio/releases/download/v0.1.11/generate-random-audio_v0.1.11_x86_64-unknown-linux-musl.tar.gz"
+      sha256 "a066c9b760010f8ac711b7c88bd4a566ed1ab789234f1633d35dc4db654aab0c"
     end
-    on_macos do
-      resource "generate-random-audio" do
-        url "https://github.com/georgettica/generate-random-audio/releases/download/v0.1.11/generate-random-audio_v0.1.11_x86_64-apple-darwin.zip"
-        sha256 "0072a0911eb070fa68d74573c9f233bc7dfa51ccb29eca04da79a9534e91542d"
-      end
-    end
-    yield
   end
+  on_macos do
+    resource "generate-random-audio" do
+      url "https://github.com/georgettica/generate-random-audio/releases/download/v0.1.11/generate-random-audio_v0.1.11_x86_64-apple-darwin.zip"
+      sha256 "0072a0911eb070fa68d74573c9f233bc7dfa51ccb29eca04da79a9534e91542d"
+    end
+  end
+  
 
   test do
-    system bin/"audiowaveform", "--help"
-    retrieve_audio_generator do
+    audiowaveform = bin/"audiowaveform"
+    system audiowaveform, "--help"
+    resource("generate-random-audio").stage do
       system "./generate-random-audio"
       assert "random_audio.wav", :exists?
     end
-    system bin/"audiowaveform", "-i", "random_audio.wav", "-o", "random_audio.png"
+    system audiowaveform, "-i", "random_audio.wav", "-o", "random_audio.png"
     assert "random_audio.png", :exists?
   end
 end
